2568e129b26976c828f0859e470fb97febf452da
IgorVictor <magusdovento@gmail.com>
2016-07-30 10:09:32 -0300

    Added missing tests

--- src/test/java/TestDirlididi.java
+++ src/test/java/TestDirlididi.java
@@ -29,6 +29,7 @@ public class TestDirlididi {
 		server.close();
 	}
 
+	//GET /PROBLEM
 	public static void TestGetProblems() {
 
 		Response response = server.newRequest("/problem").request().buildGet()
@@ -36,7 +37,27 @@ public class TestDirlididi {
 
 		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
 	}
+	
+	//POST /PROBLEM
+	public static void TestPostNewProblem() {
+		Entity<String> someEntity = Entity.entity(
+				"name, description, code, tip", MediaType.APPLICATION_JSON);
+		Response response = server
+				.newRequest("/problem").request()
+				.buildPost(someEntity).invoke();
 
+		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+	}
+
+	//GET /PROBLEM/{id}
+	public static void TEstGetProblemWithId() {
+		Response response = server.newRequest("/problem/" + PROBLEM_ID)
+				.request().buildGet().invoke();
+
+		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+	}
+
+	//GET /PROBLEM/{id}/TEST
 	public static void TestGetProblemTests() {
 		Response response = server
 				.newRequest("/problem/" + PROBLEM_ID + "/test").request()
@@ -45,6 +66,7 @@ public class TestDirlididi {
 		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
 	}
 
+	//GET /PROBLEM/{id}/TEST/{testid}
 	public static void TestGetProblemTestWithId() {
 		Response response = server
 				.newRequest("/problem/" + PROBLEM_ID + "/test/" + TEST_ID)
@@ -53,6 +75,7 @@ public class TestDirlididi {
 		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
 	}
 
+	//POST /PROBLEM/{id}/SOLUTION
 	public static void TestPostSolutionToProblem() {
 		Entity<String> someEntity = Entity.entity("solutionString, user",
 				MediaType.APPLICATION_JSON);
@@ -63,11 +86,23 @@ public class TestDirlididi {
 		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
 	}
 
+	//POST /PROBLEM/{id}/TEST
 	public static void TestPostNewProblemTest() {
 		Entity<String> someEntity = Entity.entity(
 				"name, tip, entry, expectedResult", MediaType.APPLICATION_JSON);
 		Response response = server
 				.newRequest("/problem/" + PROBLEM_ID + "/test").request()
+				.buildPost(someEntity).invoke();
+
+		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+	}
+	
+	//POST /LOGIN
+	public static void TestLogin() {
+		Entity<String> someEntity = Entity.entity(
+				"login, pass", MediaType.APPLICATION_JSON);
+		Response response = server
+				.newRequest("/login").request()
 				.buildPost(someEntity).invoke();
 
 		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
